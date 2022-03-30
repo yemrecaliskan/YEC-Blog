436be916f8d40208f3eb123641b4552413598d66
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2022-03-30 17:08:57 +0300

    Login İşlemi

--- YEC-Blog/Controllers/LoginController.cs
+++ YEC-Blog/Controllers/LoginController.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace YEC_Blog.Controllers
@@ -14,5 +19,39 @@ namespace YEC_Blog.Controllers
         {
             return View();
         }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Index(Writer p)
+        {
+            Context c = new Context();
+            var dataValue = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
+            if(dataValue != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,p.WriterMail)
+                };
+                var userIdentity = new ClaimsIdentity(claims,"a");
+                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+                await HttpContext.SignInAsync(principal);
+                return RedirectToAction("Index", "Writer");
+            }
+            else
+            {
+                return View();
+            }
+        }
     }
 }
+
+//Context c = new Context();
+//var dataValue = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
+//if (dataValue != null)
+//{
+//    HttpContext.Session.SetString("username", p.WriterMail);
+//    return RedirectToAction("Index", "Writer");
+//}
+//else
+//{
+//    return View();
+//}
