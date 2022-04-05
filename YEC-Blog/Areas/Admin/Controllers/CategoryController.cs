9a793f7659a11a1798151e272a6ed4f32e4aa17a
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2022-04-05 16:11:52 +0300

    Pagination

--- YEC-Blog/Areas/Admin/Controllers/CategoryController.cs
+++ YEC-Blog/Areas/Admin/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace YEC_Blog.Areas.Admin.Controllers
 {
@@ -12,9 +13,9 @@ namespace YEC_Blog.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         CategoryManager cm = new CategoryManager(new EfCategoryRepository());
-        public IActionResult Index()
+        public IActionResult Index(int page=1)
         {
-            var values = cm.GetList();
+            var values = cm.GetList().ToPagedList(page, 3);
             return View(values);
         }
     }
--- YEC-Blog/Startup.cs
+++ YEC-Blog/Startup.cs
@@ -73,13 +73,14 @@ namespace YEC_Blog
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-                endpoints.MapControllerRoute(
                     name: "areas",
                     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
                   );
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                
             });
         }
     }
