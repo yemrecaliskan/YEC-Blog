1e0948b1cf5854f5f10ed7329934035ba5cb141f
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2022-04-06 15:28:59 +0300

    İstatistik ve admin tablosu

---
+++ YEC-Blog/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YEC_Blog.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1 : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.BlogCount = bm.GetList().Count();
+            ViewBag.ContactCount = c.Contacts.Count();
+            ViewBag.CommentCount = c.Comments.Count();
+            return View();
+        }
+    }
+}
---
+++ YEC-Blog/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YEC_Blog.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2 : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            //ViewBag.BlogCount = bm.GetList().Count();
+            ViewBag.LastBlogName = c.Blogs.OrderByDescending(x=>x.BlogID).Select(x=>x.BlogTitle).Take(1).FirstOrDefault();
+            ViewBag.CommentCount = c.Comments.Count();
+            return View();
+        }
+    }
+}
