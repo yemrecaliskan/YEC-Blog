7c5c9dd800c1f76da01667a0f29688bac99ad597
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2022-04-06 14:12:42 +0300

    Excel Dinamik Tablo Export

---
+++ YEC-Blog/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,60 @@
+﻿using ClosedXML.Excel;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using YEC_Blog.Areas.Admin.Models;
+
+namespace YEC_Blog.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogController : Controller
+    {
+        public IActionResult ExportExcelBlogList()
+        {
+            using (var workBook = new XLWorkbook())
+            {
+                var workSheet = workBook.Worksheets.Add("Blog Listesi");
+                workSheet.Cell(1, 1).Value = "Blog ID";
+                workSheet.Cell(1, 2).Value = "Blog Adı";
+
+                int BlogRowCount = 2;
+                foreach (var item in GetBlogList())
+                {
+                    workSheet.Cell(BlogRowCount, 1).Value = item.ID;
+                    workSheet.Cell(BlogRowCount, 2).Value = item.BlogName;
+                    BlogRowCount++;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workBook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application / vnd.openxmlformats-officedocument.spreadsheetml.sheet", "BlogList.xlsx");
+                }
+            }
+        }
+
+        public List<BlogModel> GetBlogList()
+        {
+            List<BlogModel> bm = new List<BlogModel>();
+            using (var c = new Context())
+            {
+                bm = c.Blogs.Select(x => new BlogModel
+                {
+                    ID = x.BlogID,
+                    BlogName = x.BlogTitle
+                }).ToList();
+            }
+            return bm;
+        }
+
+        public IActionResult BlogListExcel()
+        {
+            return View();
+        }
+    }
+}
---
+++ YEC-Blog/Areas/Admin/Models/BlogModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YEC_Blog.Areas.Admin.Models
+{
+    public class BlogModel
+    {
+        public int ID { get; set; }
+        public string BlogName { get; set; }
+    }
+}
