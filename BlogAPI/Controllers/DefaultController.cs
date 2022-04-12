2d46d0d3e82ff9bcf0f1ae5685b290b0bb327ef0
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2022-04-12 14:11:37 +0300

    Swagger Ve Api Get List

---
+++ BlogAPI/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using BlogAPI.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult EmployeeList()
+        {
+            using var c = new Context();
+            var values = c.Employees.ToList();
+            return Ok(values);
+        }
+    }
+}
