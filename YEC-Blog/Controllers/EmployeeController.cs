9735a498901c188619b586a409e98e2160f4ece4
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2022-04-12 15:38:59 +0300

    Apilerle Projenin Haberleştirilmesi Proje Üzerinden Edit & delete

--- YEC-Blog/Controllers/EmployeeController.cs
+++ YEC-Blog/Controllers/EmployeeController.cs
@@ -19,11 +19,13 @@ namespace YEC_Blog.Controllers
             var values = JsonConvert.DeserializeObject<List<Class1>>(jsonString);
             return View(values);
         }
+
         [HttpGet]
         public IActionResult AddEmployee()
         {
             return View();
         }
+
         [HttpPost]
         public async Task<IActionResult> AddEmployee(Class1 p)
         {
@@ -36,6 +38,46 @@ namespace YEC_Blog.Controllers
                 return RedirectToAction("Index","Employee");
             }
             return View(p);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditEmployee(int id)
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync("https://localhost:44385/api/Default/"+id);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonEmployee = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<Class1>(jsonEmployee);
+                return View(values);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditEmployee(Class1 p)
+        {
+            var httpClient = new HttpClient();
+            var jsonEmployee = JsonConvert.SerializeObject(p);
+            var content = new StringContent(jsonEmployee,Encoding.UTF8,"application/json");
+            var responseMessage = await httpClient.PutAsync("https://localhost:44385/api/Default", content);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.DeleteAsync("https://localhost:44385/api/Default/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+            
         }
     }
 
