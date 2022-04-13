3ccc3de652cdd038f3b08f586310a5e74dcdee65
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2022-04-13 16:33:49 +0300

    Identity ile Login İşlemi

---
+++ YEC-Blog/Models/UserSignInViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YEC_Blog.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage ="Lütfen kullanıcı adını boş bırakmayınız.")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Lütfen şifreyi boş bırakmayınız.")]
+        public string Password { get; set; }
+    }
+}
