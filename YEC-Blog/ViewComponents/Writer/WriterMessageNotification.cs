34c497148726077c2a8cfcb32eb9394eecd6bef5
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2022-04-20 17:48:16 +0300

    Yazar İşlemlerinin Tamamlanması

--- YEC-Blog/ViewComponents/Writer/WriterMessageNotification.cs
+++ YEC-Blog/ViewComponents/Writer/WriterMessageNotification.cs
@@ -1,5 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,10 +13,18 @@ namespace YEC_Blog.ViewComponents.Writer
     public class WriterMessageNotification : ViewComponent
     {
         Message2Manager mm = new Message2Manager(new EfMessage2Repository());
-        public IViewComponentResult Invoke()
+        private readonly UserManager<AppUser> _userManager;
+
+        public WriterMessageNotification(UserManager<AppUser> userManager)
         {
-            int id = 2;
-            var values = mm.GetInboxListByWriter(id);
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.GetUserAsync((System.Security.Claims.ClaimsPrincipal)User);
+            int userId = user.Id;
+            var values = mm.GetInboxListByWriter(userId);
             return View(values);
         }
     }
