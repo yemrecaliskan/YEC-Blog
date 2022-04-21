76ecc7fd2862ce3c2983b642414333a0c71bf080
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2022-04-21 16:12:09 +0300

    Admin Paneli Düzenlemeleri ve Mesaj Sayfası

---
+++ YEC-Blog/Areas/Admin/Controllers/AdminMessageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YEC_Blog.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminMessageController : Controller
+    {
+        public IActionResult Inbox()
+        {
+            return View();
+        }
+    }
+}
