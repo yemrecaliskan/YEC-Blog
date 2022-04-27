7e8e76035c7bffaf49c4c2ae5762db5571fad905
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2022-04-27 18:50:46 +0300

    Admin Paneli Yorum Listesi

--- BusinessLayer/Abstract/ICommentService.cs
+++ BusinessLayer/Abstract/ICommentService.cs
@@ -10,9 +10,8 @@ namespace BusinessLayer.Abstract
     public interface ICommentService
     {
         void AddComment(Comment comment);
-        //void DeleteComment(Comment comment);
-        //void UpdateComment(Comment comment);
         List<Comment> GetList(int id);
-        //Comment GetById(int id);
+        List<Comment> GetCommentListWithBlog();
+
     }
 }
