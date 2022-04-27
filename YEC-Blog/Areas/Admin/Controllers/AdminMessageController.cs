781d3363c4140ed314e5f6a6f7be52d502c2fa9b
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2022-04-27 17:03:24 +0300

    Admin Paneli Giden Kutusu ve Yeni Mesaj Sayfası

--- YEC-Blog/Areas/Admin/Controllers/AdminMessageController.cs
+++ YEC-Blog/Areas/Admin/Controllers/AdminMessageController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +12,27 @@ namespace YEC_Blog.Areas.Admin.Controllers
     [Area("Admin")]
     public class AdminMessageController : Controller
     {
+        Message2Manager mm = new Message2Manager(new EfMessage2Repository());
+        Context c = new Context();
         public IActionResult Inbox()
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            var values = mm.GetInboxListByWriter(writerID);
+            return View(values);
+        }
+
+        public IActionResult Sendbox()
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            var values = mm.GetSendboxListByWriter(writerID);
+            return View(values);
+        }
+
+        public IActionResult ComposeMessage()
         {
             return View();
         }
