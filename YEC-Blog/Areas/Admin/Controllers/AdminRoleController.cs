00b4605dd979fce25965c397976ae5a6b83c89bb
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2022-04-28 13:06:56 +0300

    Rol Listesi ve Rol Ekleme

---
+++ YEC-Blog/Areas/Admin/Controllers/AdminRoleController.cs
@@ -0,0 +1,59 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YEC_Blog.Models;
+
+namespace YEC_Blog.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminRoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public AdminRoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _roleManager.Roles.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddRole()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> AddRole(RoleViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                AppRole role = new AppRole
+                {
+                    Name = model.Name
+                };
+
+                var result = await _roleManager.CreateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+            return View(model);
+        }
+    }
+}
